73d76ff6d331383d22c26fde7e34aa2b39384e22
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-04-04 17:44:17 -0300

    feat: create a more complete report

--- inventory_report/reports/complete_report.py
+++ inventory_report/reports/complete_report.py
@@ -1,2 +1,28 @@
-class CompleteReport:
-    pass
+from inventory_report.reports.simple_report import SimpleReport
+from typing import List, Dict
+from datetime import date, datetime as dt
+from collections import Counter
+
+
+class CompleteReport(SimpleReport):
+    def generate(param: List[Dict]):
+        min_date = min(d['data_de_fabricacao'] for d in param)
+        expiration_date = min(d['data_de_validade'] for d in param
+                              if dt.strptime(d['data_de_validade'], "%Y-%m-%d")
+                              .date()
+                              > date.today())
+        counter = Counter(x['nome_da_empresa']
+                          for x in param)
+
+        [(company, _)] = counter.most_common(1)
+
+        company_str = ""
+
+        for k, v in counter.items():
+            company_str += f"- {k}: {v}\n"
+
+        return f"""Data de fabricação mais antiga: {min_date}
+Data de validade mais próxima: {expiration_date}
+Empresa com mais produtos: {company}
+Produtos estocados por empresa:
+{company_str}"""
