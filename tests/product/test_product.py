540b6ef350c316580c3be421f418a24d343af8fe
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-04-04 16:45:50 -0300

    test: test the Product class constructor

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,13 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(1, 'Faca Tramontina', 'Tramontina', '12/03/1999', 
+                      'sem validade', 1324, 'Cuidar para não se cortar')
+    assert product.id == 1
+    assert product.data_de_validade == 'sem validade'
+    assert product.nome_da_empresa == 'Tramontina'
+    assert product.nome_do_produto == 'Faca Tramontina'
+    assert product.data_de_fabricacao == '12/03/1999'
+    assert product.numero_de_serie == 1324
+    assert product.instrucoes_de_armazenamento == 'Cuidar para não se cortar'
