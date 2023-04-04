4ee98f61c88a869e55e4994b5e4359fda6a65e3d
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-04-04 16:46:36 -0300

    feat: return a string with important values

--- inventory_report/reports/simple_report.py
+++ inventory_report/reports/simple_report.py
@@ -1,2 +1,19 @@
+from typing import List, Dict
+from datetime import date, datetime as dt
+from collections import Counter
+
+
 class SimpleReport:
-    pass
+    @staticmethod
+    def generate(param: List[Dict]) -> str:
+        min_date = min(d['data_de_fabricacao'] for d in param)
+        expiration_date = min(d['data_de_validade'] for d in param
+                              if dt.strptime(d['data_de_validade'], "%Y-%m-%d")
+                              .date()
+                              > date.today())
+        [(company, _)] = Counter(x['nome_da_empresa']
+                                 for x in param).most_common(1)
+
+        return f"""Data de fabricação mais antiga: {min_date}
+Data de validade mais próxima: {expiration_date}
+Empresa com mais produtos: {company}"""
