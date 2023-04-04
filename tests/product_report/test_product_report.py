cf562cd8d4c2154cf7aca6495dcb1660009b108b
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-04-04 20:32:09 -0300

    test: test_relatorio_produto implemented

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,11 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+
+string_first = "O produto PS5 fabricado em 12/03/2022 por Sony com validade"
+string_second = " até 12/03/3000 precisa ser armazenado Na minha casa."
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(1, 'PS5', 'Sony', '12/03/2022', '12/03/3000',
+                      1234, 'Na minha casa')
+    assert product.__repr__() == string_first + string_second
