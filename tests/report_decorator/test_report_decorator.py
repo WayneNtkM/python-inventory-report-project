8fe26eb9ede3f460faa91bab9781d51b28c40ce7
Wayne Nacle <100382524+WayneNtkM@users.noreply.github.com>
2023-06-05 15:16:15 -0300

    test: test_decorar_relatorio implemented

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,34 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
+import sys
 
 
-def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+products_list = [
+    {
+        "id": 1,
+        "nome_do_produto": "Cafe",
+        "nome_da_empresa": "Cafes Nature",
+        "data_de_fabricacao": "2020-07-04",
+        "data_de_validade": "2023-12-09",
+        "numero_de_serie": "FR48",
+        "instrucoes_de_armazenamento": "instrucao"
+    },
+    {
+        "id": 2,
+        "nome_do_produto": "CADEIRA",
+        "nome_da_empresa": "Forces of Nature",
+        "data_de_fabricacao": "2022-04-04",
+        "data_de_validade": "2023-12-12",
+        "numero_de_serie": "FR48",
+        "instrucoes_de_armazenamento": "Conservar em local fresco"
+    }
+]
+
+
+def test_decorar_relatorio(capsys):
+    sys.stdout.write(ColoredReport(SimpleReport).generate(products_list))
+
+    test, _ = capsys.readouterr()
+
+    assert test == 'OLSKDKS'
